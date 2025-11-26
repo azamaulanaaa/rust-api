f2841fe537306965ab6f3e32554abcdb3db3a13a
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-26 14:50:56 +0700

    feat: add verbose mode

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use clap::Parser;
 use simple_logger::SimpleLogger;
 
+#[derive(clap::Parser, Debug)]
+#[command(version)]
+struct Args {
+    #[arg(long, default_value_t = false, help = "enable verbose")]
+    verbose: bool,
+}
+
 fn main() -> anyhow::Result<()> {
-    SimpleLogger::new().init()?;
+    let args = Args::try_parse()?;
+
+    init_logger(args.verbose)?;
 
     log::info!("Hello, world!");
+
+    Ok(())
+}
+
+fn init_logger(verbose: bool) -> anyhow::Result<()> {
+    let log_level = if verbose {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
+
+    SimpleLogger::new().with_level(log_level).init()?;
 
     Ok(())
 }
