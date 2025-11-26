adde83fd3f855c85cee36de2509a284b4a6cc25e
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-26 15:28:02 +0700

    feat: add simple http endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
+
+use actix_web::{App, HttpServer};
 use clap::Parser;
 use simple_logger::SimpleLogger;
+
+mod route;
 
 #[derive(clap::Parser, Debug)]
 #[command(version)]
@@ -8,12 +13,19 @@ struct Args {
     verbose: bool,
 }
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let args = Args::try_parse()?;
 
     init_logger(args.verbose)?;
 
-    log::info!("Hello, world!");
+    let listen_addr = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 3000);
+    let listener = TcpListener::bind(listen_addr)?;
+
+    HttpServer::new(move || App::new().configure(route::config))
+        .listen(listener)?
+        .run()
+        .await?;
 
     Ok(())
 }
---
+++ src/route/mod.rs
@@ -0,0 +1,7 @@
+mod health;
+
+use actix_web::web;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(health::health);
+}
